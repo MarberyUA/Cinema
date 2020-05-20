a99c34cfa8c752423e1490874f1a30eb44fdb9a1
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-05-20 13:52:28 +0300

    simple starter template

---
+++ src/main/java/com/dev/cinema/model/Movie.java
@@ -0,0 +1,31 @@
+package com.dev.cinema.model;
+
+public class Movie {
+    Long id;
+    String title;
+    String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
